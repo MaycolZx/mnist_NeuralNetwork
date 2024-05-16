070d4482fcafe7511f4a1152088f0e535146de9d
Maycol-Zx-PC <alexander51600@hotmail.com>
2024-05-16 00:16:11 -0500

    first step

--- principalFile/pruebaNuevaCsv.cpp
+++ principalFile/pruebaNuevaCsv.cpp
@@ -1,160 +1,298 @@
-#pragma once
 #include <cmath>
+#include <cstdlib>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <math.h>
 #include <random>
+#include <sstream>
 #include <string>
 #include <vector>
+
 using namespace std;
 
-vector<vector<float>> vamosAcrearVector(int indexV) {
-    vector<vector<float>> inputVector;
-    std::ifstream archivo("E:\\DOWNLOAD\\mnist.csv");
-    std::string linea;
+// Función para leer un elemento de un archivo CSV basado en su índice
+void leerElementoCSV(const std::string &nombreArchivo, int indiceColumna,
+                     std::vector<int> &resultados) {
+  std::ifstream archivo(nombreArchivo);
+  std::string linea;
 
-    // while (std::getline(archivo, linea)) {
-    std::getline(archivo, linea);
-    std::vector<float> numeros;
-    std::string numero_str;
-    for (char caracter : linea) {
-        if (caracter != ',') {
-            numero_str += caracter;
-        }
-        else {
-            numeros.push_back(std::stoi(numero_str));
-            numero_str = "";
-        }
-    }
-    numeros.push_back(std::stoi(numero_str));
-    // for (int numero : numeros) {
-    //   std::cout << numero << " ";
-    // }
-    // std::cout << std::endl;
-    for (int numero : numeros) {
-        // std::cout << numero << " ";
-        vector<float> valoresIngresoM;
-        valoresIngresoM.push_back(numero);
-        inputVector.push_back(valoresIngresoM);
-    }
-    std::cout << std::endl;
+  while (std::getline(archivo, linea)) {
+    std::stringstream ss(linea);
+    std::string elemento;
+    int indice = 0;
 
+    while (std::getline(ss, elemento, ',')) {
+      if (indice == indiceColumna) {
+        try {
+          int valor = std::stoi(elemento);
+          resultados.push_back(valor);
+        } catch (const std::exception &e) {
+          std::cerr << "Error al convertir a entero: " << e.what() << std::endl;
+          // Puedes manejar el error de conversión aquí
+        }
+        break;
+      }
+      indice++;
+    }
+  }
+
+  archivo.close();
+}
+
+// Función para escribir un vector<vector<float>> en un archivo CSV
+void escribirCSV(const vector<vector<float>> &datos,
+                 const string &nombreArchivo) {
+  ofstream archivo(nombreArchivo);
+  if (archivo.is_open()) {
+    for (const auto &fila : datos) {
+      for (size_t i = 0; i < fila.size(); ++i) {
+        archivo << fila[i];
+        if (i != fila.size() - 1)
+          archivo << ",";
+      }
+      archivo << endl;
+    }
     archivo.close();
-    return inputVector;
+    cout << "Datos guardados en " << nombreArchivo << endl;
+  } else {
+    cout << "No se pudo abrir el archivo " << nombreArchivo
+         << " para escritura." << endl;
+  }
+}
+
+// Función para leer valores flotantes desde un archivo CSV
+vector<vector<float>> leerCSV(const string &nombreArchivo) {
+  vector<vector<float>> datos;
+  ifstream archivo(nombreArchivo);
+  if (archivo.is_open()) {
+    string linea;
+    while (getline(archivo, linea)) {
+      stringstream ss(linea);
+      vector<float> fila;
+      string valor;
+      while (getline(ss, valor, ',')) {
+        fila.push_back(stof(valor));
+      }
+      datos.push_back(fila);
+    }
+    archivo.close();
+    cout << "Datos leídos desde " << nombreArchivo << endl;
+  } else {
+    cout << "No se pudo abrir el archivo " << nombreArchivo << " para lectura."
+         << endl;
+  }
+  return datos;
 }
 
 vector<vector<float>> vectoresRandomPesos(int limitNeurons = 10,
-    int precision = 2) {
-    vector<vector<float>> pesosRandom;
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+                                          int precision = 2) {
+  vector<vector<float>> pesosRandom;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<float> dis(0.0f, 1.0f);
 
-    for (int j = 0; j < limitNeurons; j++) {
-        vector<float> pesosVector;
-        for (int i = 0; i < 784; i++) {
-            float numero_aleatorio = dis(gen) * 100.0f / 100.0f;
-            pesosVector.push_back(numero_aleatorio);
-        }
-        pesosRandom.push_back(pesosVector);
+  for (int j = 0; j < limitNeurons; j++) {
+    vector<float> pesosVector;
+    for (int i = 0; i < 784; i++) {
+      float numero_aleatorio = dis(gen) * 100.0f / 100.0f;
+      pesosVector.push_back(numero_aleatorio);
     }
-    // Que pesos tiene :Print()
-      //for (int i = 0; i < 10; i++) {
-      //  for (int j = 0; j < 15; j++) {
-      //    cout << std::fixed << std::setprecision(2) << pesosRandom[i][j] << "";
-      //  }
-      //  cout << endl;
-      //}
-    return pesosRandom;
+    pesosRandom.push_back(pesosVector);
+  }
+  // Que pesos tiene :Print()
+  // for (int i = 0; i < 10; i++) {
+  //  for (int j = 0; j < 15; j++) {
+  //    cout << std::fixed << std::setprecision(2) << pesosRandom[i][j] << "";
+  //  }
+  //  cout << endl;
+  //}
+  return pesosRandom;
 }
 
 float sigmoide(float x) { return 1.0f / (1.0f + std::exp(-x)); }
 
-//bool reLU(int valor) { return valor > 0; }
+// bool reLU(int valor) { return valor > 0; }
 
+// float reLU(float x) { return (x > 0) ? x : 0; }
 float reLU(float x) {
-    return (x > 0) ? x : 0;
+  if (x > 14000) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 float precisionEntrenamiento(float numeroCorrecto, float numeroTotal) {
-    float precisionE = numeroCorrecto / numeroTotal;
-    return precisionE;
+  float precisionE = numeroCorrecto / numeroTotal;
+  return precisionE;
 }
 
-vector<int> recalculandoPesos(const vector<vector<float>>& matxPesos, const vector<vector<float>>& inputN,const vector<vector<float>>& resulFunct) {
-// Peso = peso + n(t-o)*xi 
-    int valorTivo = inputN[0][0];
-    float tazaAp = 0.5;
-    vector <int> indexSearch = {};
-    for (int i = 0; i < resulFunct.size(); i++) {
-        float valActivation = reLU(resulFunct[i][0]);
-        cout << "#" << i << "-valor de Act: " << valActivation << endl;
-        if ((valorTivo == i && valActivation == 0) || (valActivation == 1 && i != valorTivo)) {
-            indexSearch.push_back(i);
-        }
+vector<int> recalculandoPesos(vector<vector<float>> &matxPesos,
+                              vector<vector<float>> &inputN,
+                              vector<vector<float>> &resulFunct) {
+  // Peso = peso + n(t-o)*xi
+  int valorTivo = inputN[0][0];
+  // cout << "Valor objetivo: " << valorTivo << endl;
+  float tazaAp = 0.5;
+  vector<int> indexSearch = {};
+  cout << "Tamano index: " << indexSearch.size() << endl;
+  for (int i = 0; i < resulFunct.size(); i++) {
+    float valActivation = reLU(resulFunct[i][0]);
+    cout << "#" << i << "-valor de Act: " << valActivation << endl;
+    // if ((valorTivo == i && valActivation == 0) ||
+    //     (valActivation == 1 && i != valorTivo)) {
+    //   indexSearch.push_back(i);
+    // }
+    if (valorTivo == i && valActivation == 0) {
+      indexSearch.push_back(i);
     }
-    if (!indexSearch.empty()) {
-        for (int i = 0; i < indexSearch.size(); i++) {
-            float valActivation = reLU(resulFunct[i][0]);
-            vector<float> valorPesosIndex = matxPesos[i];
-            for (int k = 0; k < valorPesosIndex.size(); k++) {
-                valorPesosIndex[k] = valorPesosIndex[k] + (tazaAp * (0 - valActivation) * inputN[k + 1][0]);
-            }
-        }
+    if (valActivation == 1 && i != valorTivo) {
+      indexSearch.push_back(i);
     }
-    return indexSearch;
+  }
+  cout << "Tamano index-Segundo: " << indexSearch.size() << endl;
+  // system("clear");
+  int valorWaobt = 0;
+  if (!indexSearch.empty()) {
+    for (int i = 0; i < indexSearch.size(); i++) {
+      cout << "Valor del index: " << indexSearch[i] << endl;
+      if (indexSearch[i] == valorTivo) {
+        valorWaobt = 1;
+      } else {
+        valorWaobt = 0;
+      }
+      float valActivation = reLU(resulFunct[indexSearch[i]][0]);
+      // vector<float> valorPesosIndex = matxPesos[i];
+      for (int k = 0; k < matxPesos[i].size(); k++) {
+        matxPesos[indexSearch[i]][k] =
+            matxPesos[indexSearch[i]][k] +
+            (tazaAp * (valorWaobt - valActivation) * inputN[k + 1][0]);
+      }
+    }
+  }
+  return indexSearch;
 }
 
 // Función para multiplicar dos matrices representadas como vectores de vectores
 std::vector<std::vector<float>>
-multiplicarMatrices(const std::vector<std::vector<float>>& matriz1,
-    const std::vector<std::vector<float>>& matriz2) {
-    float biasV = 1;
-    int filas_matriz1 = matriz1.size();
-    int columnas_matriz1 = matriz1[0].size();
-    int filas_matriz2 = matriz2.size() - 1;
-    int columnas_matriz2 = matriz2[0].size();
-    cout << "Matriz 01 es : " << filas_matriz1 << " x " << columnas_matriz1
-        << endl;
-    cout << "Matriz 02 es: " << filas_matriz2 << " x " << columnas_matriz2
-        << endl;
+multiplicarMatrices(std::vector<std::vector<float>> &matriz1,
+                    std::vector<std::vector<float>> &matriz2) {
+  float biasV = 1;
+  int filas_matriz1 = matriz1.size();
+  int columnas_matriz1 = matriz1[0].size();
+  int filas_matriz2 = matriz2.size() - 1;
+  int columnas_matriz2 = matriz2[0].size();
+  cout << "Matriz 01 es : " << filas_matriz1 << " x " << columnas_matriz1
+       << endl;
+  cout << "Matriz 02 es: " << filas_matriz2 << " x " << columnas_matriz2
+       << endl;
 
-    // Verificar que las matrices sean multiplicables
-    if (columnas_matriz1 != filas_matriz2) {
-        std::cerr << "Error: Las matrices no son compatibles para multiplicación."
-            << std::endl;
-        return std::vector<std::vector<float>>(); // Devolver matriz vacía en caso
-        // de error
+  // Verificar que las matrices sean multiplicables
+  if (columnas_matriz1 != filas_matriz2) {
+    std::cerr << "Error: Las matrices no son compatibles para multiplicación."
+              << std::endl;
+    return std::vector<std::vector<float>>(); // Devolver matriz vacía en caso
+                                              // de error
+  }
+
+  // Crear la matriz resultado inicializada con ceros
+  float opResult = 0.0f;
+
+  int epocIndex = 0;
+  std::vector<std::vector<float>> resultado(
+      filas_matriz1, std::vector<float>(columnas_matriz2, 0.0f));
+
+  while (true) {
+    // Realizar la multiplicación de matrices
+    for (int i = 0; i < filas_matriz1; ++i) {
+      for (int j = 0; j < columnas_matriz2; ++j) {
+        resultado[i][j] = 0.0f;
+      }
     }
 
-    // Crear la matriz resultado inicializada con ceros
-    std::vector<std::vector<float>> resultado(
-        filas_matriz1, std::vector<float>(columnas_matriz2, 0.0f));
-    float opResult = 0.0f;
-
-    int epocIndex = 0;
-    while (true) {
-        // Realizar la multiplicación de matrices
-        cout << "Epoca-#"  << epocIndex << endl;
-        for (int i = 0; i < filas_matriz1; ++i) {
-            for (int j = 0; j < columnas_matriz2; ++j) {
-                for (int k = 1; k < columnas_matriz1; ++k) {
-                    resultado[i][j] += matriz1[i][k] * matriz2[k][j];
-                }
-                resultado[i][j] += biasV;
-                cout << "Un resultado es: " << resultado[i][j] << endl;
-            }
-            // resultado[i].push_back();
+    cout << "Epoca-#" << epocIndex << endl;
+    for (int i = 0; i < filas_matriz1; ++i) {
+      for (int j = 0; j < columnas_matriz2; ++j) {
+        for (int k = 0; k < columnas_matriz1; ++k) {
+          resultado[i][j] += matriz1[i][k] * matriz2[k + 1][j];
         }
-        vector<int> rCond = recalculandoPesos(matriz1, matriz2, resultado);
-        if (rCond.empty()) {
-            break;
-        }
-        epocIndex++;
+        resultado[i][j] += biasV;
+        // cout << "Un resultado es: " << resultado[i][j] << endl;
+        cout << "#-" << i << " " << fixed << setprecision(8) << resultado[i][j]
+             << endl;
+      }
+      // resultado[i].push_back();
     }
-    
-    return resultado;
+    vector<int> rCond = recalculandoPesos(matriz1, matriz2, resultado);
+    if (rCond.size() == 0) {
+      break;
+    }
+    epocIndex++;
+    // system("sleep 2");
+  }
+  // vector<vector<float>> datos = {
+  //     {1.2, 3.4, 5.6}, {7.8, 9.0, 1.2}, {3.4, 5.6, 7.8}};
+  string nombreArchivo = "datosPesos.csv";
+
+  escribirCSV(matriz1, nombreArchivo);
+  // vector<vector<float>> datosLeidos = leerCSV(nombreArchivo);
+
+  // Imprimir los datos leídos
+  // for (const auto &fila : datosLeidos) {
+  //   for (const auto &valor : fila) {
+  //     cout << valor << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  vector<vector<float>> valorTmpR;
+  return valorTmpR;
+}
+
+void vamosAcrearVector() {
+  // std::ifstream archivo("E:\\DOWNLOAD\\mnist.csv");//Descomentar de estas en
+  // window y cambiar de direccion
+  vector<vector<float>> valoresRpesos = vectoresRandomPesos();
+  std::ifstream archivo("./mnist.csv");
+  std::string linea;
+
+  // int contadorTMP = 0;
+  while (std::getline(archivo, linea)) {
+    // std::getline(archivo, linea);
+    vector<vector<float>> inputVector;
+    vector<float> numeros;
+    string numero_str;
+    for (char caracter : linea) {
+      if (caracter != ',') {
+        numero_str += caracter;
+      } else {
+        numeros.push_back(std::stoi(numero_str));
+        numero_str = "";
+      }
+    }
+    numeros.push_back(std::stoi(numero_str));
+    for (int numero : numeros) {
+      vector<float> valoresIngresoM;
+      valoresIngresoM.push_back(numero);
+      inputVector.push_back(valoresIngresoM);
+    }
+    // for (int i = 0; i < inputVector.size(); i++) {
+    //   for (int j = 0; j < inputVector[i].size(); j++) {
+    //     cout << inputVector[i][j] << " ";
+    //   }
+    // }
+    // cout << endl << "Este es el limite del primero " << endl;
+    //
+    cout << "El tamano del vector es: " << inputVector.size() << endl;
+    std::vector<std::vector<float>> resultado =
+        multiplicarMatrices(valoresRpesos, inputVector);
+    // contadorTMP++;
+    // if (contadorTMP == 3) {
+    //   break;
+    // }
+  }
+  archivo.close();
 }
 
 // Función para sumar dos matrices representadas como vectores de vectores
@@ -189,29 +327,41 @@ multiplicarMatrices(const std::vector<std::vector<float>>& matriz1,
 // }
 
 float costDif(int valorDeseado) {
-    float wave02[10] = {};
-    float resultadoCost = 0;
-    for (int i = 0; i < 10; i++) {
-        if (i == valorDeseado) {
-            resultadoCost += pow(wave02[i] - 1, 2);
-            continue;
-        }
-        resultadoCost += wave02[i];
+  float wave02[10] = {};
+  float resultadoCost = 0;
+  for (int i = 0; i < 10; i++) {
+    if (i == valorDeseado) {
+      resultadoCost += pow(wave02[i] - 1, 2);
+      continue;
     }
-    return resultadoCost;
+    resultadoCost += wave02[i];
+  }
+  return resultadoCost;
 }
 
 int main() {
-    vector<vector<float>> valoresRpesos = vectoresRandomPesos();
-    float functionZ[10] = {};
-    vector<vector<float>> numerosInput;
-    int indexCsv = 0;
-    numerosInput = vamosAcrearVector(indexCsv);
-    cout << "El tamano del vector es: " << numerosInput.size() << endl;
+  // vector<vector<float>> valoresRpesos = vectoresRandomPesos();
+  // vector<vector<float>> numerosInput;
+  vamosAcrearVector();
+  // for (int indexCsv = 0; indexCsv < 5; indexCsv++) {
+  //   numerosInput = vamosAcrearVector(indexCsv);
+  //   cout << "El tamano del vector es: " << numerosInput.size() << endl;
+  //   std::vector<std::vector<float>> resultado =
+  //       multiplicarMatrices(valoresRpesos, numerosInput);
+  // }
 
-    std::vector<std::vector<float>> resultado =
-        multiplicarMatrices(valoresRpesos, numerosInput);
-    return 0;
+  // std::vector<int> resultadosMIndex;
+  // std::string nombreArchivo = "mnist.csv";
+  // int indiceColumnaDeseado = 0;
+  //
+  // leerElementoCSV(nombreArchivo, indiceColumnaDeseado, resultadosMIndex);
+  //
+  // // Imprimir los valores leídos
+  // for (int valor : resultadosMIndex) {
+  //   std::cout << valor << " ";
+  // }
+  // cout << endl;
+  return 0;
 }
 
 // otherFunction
